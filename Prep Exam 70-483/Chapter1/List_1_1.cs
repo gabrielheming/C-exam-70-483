b998bac08140b1cc0805c72d8c2d84215794bdf4
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2017-11-10 09:43:30 -0200

    Removed default Program.cs file. Addes first Thread example

---
+++ Prep Exam 70-483/Chapter1/List_1_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Prep_Exam_70_483.Chapter1
+{
+    public static class List_1_1
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i< 10; i++)
+            {
+                Console.WriteLine("Thread: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work");
+                Thread.Sleep(0);
+            }
+
+            thread.Join();
+        }
+    }
+}
