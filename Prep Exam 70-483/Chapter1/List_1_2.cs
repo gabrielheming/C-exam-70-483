782a580bfe03b5d858796103e8c1da9358c38c95
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2017-11-10 10:48:38 -0200

    Added List_1_2 example

---
+++ Prep Exam 70-483/Chapter1/List_1_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Prep_Exam_70_483.Chapter1
+{
+    public static class List_1_2
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.IsBackground = true;//true = exits immediatly / false = prints ThreadMethod
+            thread.Start();
+        }
+    }
+}
